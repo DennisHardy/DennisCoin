571bae707f4809feba47ebfa7cecedc5aac41a2d
Dennis Hardy <hardy17107@gmail.com>
2018-04-25 15:51:55 -0700

    Initial Commit

---
+++ DennisCoin/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DennisCoin
+//
+//  Created by Dennis Hardy on 4/25/18.
+//  Copyright © 2018 Dennis Hardy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
